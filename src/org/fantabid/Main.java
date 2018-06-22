919d7cc591a622a9f74dda1cb6e2d793493d567f
Diego Mazzieri <diego.mazzieri@studio.unibo.it>
2018-06-22 19:10:38 +0200

    Connect to an existing database and retrieve sample data

--- src/org/fantabid/Main.java
+++ src/org/fantabid/Main.java
@@ -9,10 +9,11 @@ import java.sql.Statement;
 public class Main {
 
     public static void main(String[] args) throws SQLException {
-        String url = "jdbc:postgresql://localhost/worldb";
+        String url = "jdbc:postgresql://fantabid.czlxuwq0tdm7.us-west-2.rds.amazonaws.com/fantabidb?user=mazzio&password=crostopiada";
         Connection conn = DriverManager.getConnection(url);
         Statement s = conn.createStatement();
-        ResultSet r = s.executeQuery("SELECT * FROM country");
+//        s.executeUpdate("INSERT INTO products VALUES (1, 'Cheese', 9.99);");
+        ResultSet r = s.executeQuery("SELECT * FROM products");
         while (r.next()) {
             System.out.println(r.getString(2));
         }
