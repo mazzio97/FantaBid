3285ae46e0351af4894f4fc282a2f9207839bdba
Diego Mazzieri <diego.mazzieri@studio.unibo.it>
2018-06-26 17:40:54 +0200

    JOOQ code generation

--- src/org/fantabid/Main.java
+++ src/org/fantabid/Main.java
@@ -1,12 +1,17 @@
 package org.fantabid;
 
+import static org.fantabid.generated.Tables.*;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 import org.fantabid.view.Views;
+import org.jooq.DSLContext;
+import org.jooq.Record;
+import org.jooq.Result;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
 
 import javafx.application.Application;
 import javafx.scene.image.Image;
@@ -15,17 +20,23 @@ import javafx.stage.Stage;
 
 public final class Main extends Application {
 
+    private final static String DB_URL = "jdbc:postgresql://fantabid.czlxuwq0tdm7.us-west-2.rds.amazonaws.com/fantabidb";
+    private final static String USER = "mazzio";
+    private final static String PASSWORD = "crostopiada";
     private static Stage primaryStage;
-
+    public static DSLContext create;
+    
     public static void main(final String[] args) throws SQLException {
-        String url = "jdbc:postgresql://fantabid.czlxuwq0tdm7.us-west-2.rds.amazonaws.com/fantabidb?user=mazzio&password=crostopiada";
-        Connection conn = DriverManager.getConnection(url);
-        Statement s = conn.createStatement();
-//        s.executeUpdate("INSERT INTO products VALUES (1, 'Cheese', 9.99);");
-        ResultSet r = s.executeQuery("SELECT * FROM Calciatore");
-        while (r.next()) {
-            System.out.println(r.getString(2));
-        }
+        System.getProperties().setProperty("org.jooq.no-logo", "true");
+        Connection conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+        create = DSL.using(conn, SQLDialect.POSTGRES);
+        Result<Record> r = create.select()
+                         .from(CALCIATORE)
+                         .where(CALCIATORE.RUOLO.eq("A"))
+                         .and(CALCIATORE.SQUADRA.eq("Inter"))
+                         .fetch();
+        
+        r.forEach(rec -> System.out.println(rec.getValue(CALCIATORE.NOME) + " " + rec.getValue(CALCIATORE.SQUADRA) +  " " + rec.getValue(CALCIATORE.PREZZOSTANDARD)));
         
         launch();
     }
